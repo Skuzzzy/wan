f5170e63da7c3fcebcc7c0fd1f289589e43e2ce0
Daniel Baird <JamesDanielBaird@gmail.com>
2015-11-23 22:38:00 -0700

    moving to laptop

--- wan.c
+++ wan.c
@@ -1,14 +1,19 @@
 // Daniel Baird
 
 #include <ncurses.h>
+#include <stdlib.h>
 
 char CTRLD = 3;
+
+int row;
+int col;
 
 void setup()
 {
 	initscr();
 	raw();
 	noecho();
+	getmaxyx(stdscr,row,col);
 }
 
 void teardown()
@@ -16,9 +21,18 @@ void teardown()
 	endwin();
 }
 
+void die(char* error)
+{
+	endwin();
+	printf("%s", error);
+	exit(1);
+}
 int main (int argc, char *argv[])
 {
 	setup();
+
+	printw("%d row\n%d col\n", row, col);
+
     char ch = ' ';
 	while(ch != CTRLD)
 	{
